c3a2fd47525ca764ce89844d7f1fc6550d91b927
BhargaviChunchu <bhargavichunchu@gmail.com>
2021-05-16 16:03:23 -0700

    Add files via upload

--- src/design/align_significands.sv
+++ src/design/align_significands.sv
@@ -24,21 +24,30 @@
  */
 
 
+// Gets the second significand value and the number of bits it has to be shifted by as inputs and gives the shifted value as output
 import addpkg::*;
 
 module align_significands (
     sig2, shift, sig2_aligned
 );
 
-    input [22:0] sig2;
-    input [7:0] shift;                      // Output of the subraction of the exponents
-    output [22:0] sig2_aligned;             // Only outputting aligned op2, since op1 can just be passed through.
+input logic [22:0] sig2;
+input logic [7:0] shift;                      // Output of the subraction of the exponents
+output logic [25:0] sig2_aligned;             // Only outputting aligned op2, since op1 can just be passed through.
+bit guard, round, sticky;
+always_comb begin
+    for(int i=2; i<shift; i++) begin
+        if (sig2[i] == 1) begin
+            sticky = 1'b1;
+        end        
+    end
+    if (shift>1) begin
+        round = sig2[1];        
+    end
+    if (shift>0) begin
+        guard = sig2[0];        
+    end
+    sig2_aligned = {sig2 >> shift, sticky, round, guard};     // First shift brings hidden bit into significand (combinational)
+end
 
-    // Note: This feels like a slightly high level way of doing this? Because it involves subtraction
-    // Another way of implmementing it at a lower level is to use a while loop and shift the significand and
-    // add one to the exponent until the exponents of each match. Don't know which is more appropriate to use.
-    // Actually looking at the diagram this is probably fine. They use a sub ALU and a mux to feed into this module actually.
-
-    assign sig2_aligned = shift ? {1'b1, sig2} >> (shift-1): sig2;     // First shift brings hidden bit into significand
-    
 endmodule
