83e843afda12e37a785ac52d84af256185992ef3
Chuck Faber <kpfaber@gmail.com>
2021-05-20 18:01:48 -0700

    updating align again with new round algorithm

--- src/design/chuck_test_code/align_significands.sv
+++ src/design/chuck_test_code/align_significands.sv
@@ -23,10 +23,6 @@
  * 2021-05-06	CF	Added initial code. NOT YET TESTED.
  */
 
-`define GUARD 2
-`define ROUND 1
-`define STICKY 0
-
 import addpkg::*;
 
 module align_significands (
@@ -36,23 +32,39 @@ module align_significands (
 input [26:0] sig2;
 input [7:0] shift;                      // Output of the subraction of the exponents
 output [26:0] sig2_aligned;             // Only outputting aligned op2, since op1 can just be passed through.
+logic sticky;
 
-logic [23:0] sig_tmp;
-bit guard, round, sticky;
 
 always_comb begin
+
+    // if (shift > 0) begin
+    //     shifted_out = sig2[(shift-1)+3:3];
+
+    //     sig2[GUARD] = shifted_out[shift-1];
+    //     sig2[ROUND] = shifted_out[shift-2];
+    //     sig2[STICKY] = |shifted_out[shift-3:0];
+    // end
+
+    if (shift > 3) begin
+        sticky = |sig2[shift-3:0];
+    end
+
     // sig_tmp = sig2[26:3];
-    if (shift > 0) begin
-        sig2[GUARD] = sig_tmp[0];
+    // if (shift > 0) begin
+    //     sig2[GUARD] = sig_tmp[0];
+    // end
+    // if (shift > 1) begin
+    //     sig2[ROUND] = sig_tmp[1];
+    // end
+    // if (shift > 2) begin
+    //     // check if any bit between sig_tmp[shift:2] is 1
+    //     sig2[STICKY] = |sig_tmp[shift:2];
+    // end
+
+    sig2_aligned = sig2 >> shift;     // First shift brings hidden bit into significand (combinational)
+    if (shift > 3) begin
+        sig2_aligned[0] = sticky;
     end
-    if (shift > 1) begin
-        sig2[ROUND] = sig_tmp[1];
-    end
-    if (shift > 2) begin
-        // check if any bit between sig_tmp[shift:2] is 1
-        sig2[STICKY] = |sig_tmp[shift:2];
-    end
-    sig2_aligned = {sig2[26:3] >> shift, sig2[2:0]};     // First shift brings hidden bit into significand (combinational)
 end
 
 endmodule
