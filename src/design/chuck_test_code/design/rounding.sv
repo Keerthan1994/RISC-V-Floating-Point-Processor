dd4e9908190fa736aa7db210c286a291a5ef2ef2
Chuck Faber <kpfaber@gmail.com>
2021-05-23 20:55:21 -0700

    Verified Rounding Module

--- src/design/chuck_test_code/design/rounding.sv
+++ src/design/chuck_test_code/design/rounding.sv
@@ -1,25 +1,24 @@
+// 5/23/2021 - Verified by CF and BC
+
 module rounding (
-    sig_n, sig_nr
+    sig_n, sig_r, co
     );
 
     input [26:0] sig_n;
-    output [22:0] sig_nr;
+    output logic [26:0] sig_r;
+    output logic co;
     logic [24:0] sig_tmp;
 
     always_comb begin
-        if (!sig_n[2]) begin
-            sig_tmp[23:0] = sig_n[26:3];
-        end else if (!sig_n[3] && sig_n[2] && !sig_n[1] && !sig_n[0]) begin
-            sig_tmp[23:0] = sig_n[26:3];
-        end else begin
+        if (!sig_n[2]) begin                                                // Truncate Case
+            sig_tmp = sig_n[26:3];
+        end else if (!sig_n[3] && sig_n[2] && !sig_n[1] && !sig_n[0]) begin // Round to Even Case
+            sig_tmp = sig_n[26:3];
+        end else begin                                                      // Round Up Case
             sig_tmp = sig_n[26:3] + 1;
         end
-
-        if (sig_tmp[24]) begin
-            // we need to normalize
-        end else begin
-            sig_nr = sig_tmp[22:0];
-        end
+        co = sig_tmp[24];
+        sig_r = {sig_tmp[23:0], 3'b000};
     end
 
 endmodule
