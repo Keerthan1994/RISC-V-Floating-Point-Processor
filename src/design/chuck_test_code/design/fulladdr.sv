5f3350e7590644783244111abd2540c3b1f4fdb0
Chuck Faber <kpfaber@gmail.com>
2021-05-31 11:15:49 -0700

    removed time delay on full adder

--- src/design/chuck_test_code/design/fulladdr.sv
+++ src/design/chuck_test_code/design/fulladdr.sv
@@ -2,7 +2,7 @@ module fulladder(S, CO, A, B, CI);
     input wire A, B, CI;
     output logic S, CO;
 
-    assign #1 S = A ^ B ^ CI;
-    assign #1 CO = A & B | A & CI | B & CI;
+    assign S = A ^ B ^ CI;
+    assign CO = A & B | A & CI | B & CI;
     
 endmodule
