3ec29804f29b752e744c7ddfee23aa46472ef1b3
Chuck Faber <kpfaber@gmail.com>
2021-06-09 13:28:38 -0700

    moved tests into tasks, and added command line arguments

--- src/chuck_test_code/verification/add_sub_top_tb.sv
+++ src/chuck_test_code/verification/add_sub_top_tb.sv
@@ -2,9 +2,11 @@ import fp_pkg::*;
 
 parameter SIG_BITS = 23;
 parameter EXP_BITS = 8;
+
 module top();
 
 parameter NTESTS = 1000;
+parameter OP = 0;
 
 // shortreal op1, op2, out;
 shortreal out_sr;
@@ -48,18 +50,164 @@ logic out_prod_ack;         // probably not required.
 // Verification Signals
 FloatingPoint op1, op2, out, exp;
 opcode_t opc;           // opcode
+string op_arg;          // Command Line Argument ex. "vsim -c top +OP=ADD"
 
 fp_case_t op1_case, op2_case;
 logic [3:0] sign_tc;    // {opcode, op1_sign, op2_sign}
 bit err;
 
-initial 
-    forever #10 clk = ~clk;
-
 // Instantiate FP Modules
 add_sub_top ast0 (.*);
 fdiv_newton fdiv0 (.*, .err_o(err_o_div));
 fp_multiplier fmul0 (.*);
+
+// This Task Tests all 64 Corner Case Combinations, with all combinations of sign bits for the given opcode
+task testCornerCases();
+    do begin
+        do begin
+            for (sign_tc = 0; sign_tc < 4; sign_tc++) begin
+                for (int i = 0; i < NTESTS; i++) begin
+                    generateCornerCase(op1, op2, exp, op1_case, op2_case, sign_tc[1], sign_tc[0], opc);
+                    
+                    // Add/Sub Inputs
+                    opcode = bit'(opc);
+                    sign1 = op1.getSign();
+                    sign2 = op2.getSign();
+                    exp1 = op1.getExponent();
+                    exp2 = op2.getExponent();
+                    sig1 = op1.getSignificand();
+                    sig2 = op2.getSignificand();
+
+                    // Mult/Div Specific Input Handling
+                    if (opc == DIV) begin
+                        wait(!busy);
+                        a = {sign1, exp1, sig1};    // Div Input
+                        b = {sign2, exp2, sig2};    // Div Input
+                    end else if (opc == MUL) begin
+                        in_A = {sign1, exp1, sig1};                   // Mult Input
+                        in_B = {sign2, exp2, sig2};                   // Mult Input
+                        repeat (1) @(negedge clk);
+                        strb_A = 1'b1;
+                        strb_B = 1'b1;
+                        repeat (1) @(negedge clk);
+                        strb_A = 1'b0;
+                        strb_B = 1'b0;
+                    end
+
+
+                    // Op Code Specific Waits and Outputs
+                    case (opc)
+                        ADD: begin 
+                            repeat (1) @(negedge clk);
+                            out.setBits(fp_out);
+                        end
+                        SUB: begin
+                            repeat (1) @(negedge clk);
+                            out.setBits(fp_out);
+                        end
+                        MUL: begin 
+                            wait(output_prod_stb);
+                            out.setBits(output_prod);
+                        end
+                        DIV: begin
+                            repeat (30) @(negedge clk)
+                            wait(!busy);
+                            out.setBits(s);
+                        end
+                    endcase
+                    checkResults(op1, op2, out, exp, opc, err);
+                    if (err) begin 
+                        err_count += 1;
+                        $display("EXP: %0b", exp.bitsToString);
+                        $display("OUT: %0b", out.bitsToString);
+                    end
+                    checkErrorCode(exp, err_o);
+                    test_count += 1;
+                end
+            end
+            op2_case = op2_case.next;
+        end while(op2_case != op2_case.first);
+        op1_case = op1_case.next;
+    end while(op1_case != op1_case.first);
+endtask
+
+// This task randomly generates NTESTS*512 operands based on the weights provided in the FloatingPoint class.
+task testRandCases();
+    for (int i = 0; i < (NTESTS * 512); i++) begin
+        generateRandCase(op1, op2, exp, opc);
+        opcode = bit'(opc);
+        sign1 = op1.getSign();
+        sign2 = op2.getSign();
+        exp1 = op1.getExponent();
+        exp2 = op2.getExponent();
+        sig1 = op1.getSignificand();
+        sig2 = op2.getSignificand();
+
+        // Mult/Div Specific Input Handling
+        if (opc == DIV) begin
+            wait(!busy);
+            a = {sign1, exp1, sig1};    // Div Input
+            b = {sign2, exp2, sig2};    // Div Input
+        end else if (opc == MUL) begin
+            in_A = {sign1, exp1, sig1};                   // Mult Input
+            in_B = {sign2, exp2, sig2};                   // Mult Input
+            repeat (1) @(negedge clk);
+            strb_A = 1'b1;
+            strb_B = 1'b1;
+            repeat (1) @(negedge clk);
+            strb_A = 1'b0;
+            strb_B = 1'b0;
+        end
+
+
+        // Op Code Specific Waits and Outputs
+        case (opc)
+            ADD: begin 
+                repeat (1) @(negedge clk);
+                out.setBits(fp_out);
+            end
+            SUB: begin
+                repeat (1) @(negedge clk);
+                out.setBits(fp_out);
+            end
+            MUL: begin 
+                wait(output_prod_stb);
+                out.setBits(output_prod);
+            end
+            DIV: begin
+                repeat (30) @(negedge clk);
+                wait(!busy);
+                out.setBits(s);
+            end
+        endcase
+        checkResults(op1, op2, out, exp, opc, err);
+        if (err) begin 
+            err_count += 1;
+            $display("EXP: %0b", exp.bitsToString);
+            $display("OUT: %0b", out.bitsToString);
+        end
+        checkErrorCode(exp, err_o);
+        test_count += 1;
+    end
+endtask
+
+// Clocking Block
+initial begin
+    forever #10 clk = ~clk;
+end
+
+initial begin
+    if ($value$plusargs ("OP=%s", op_arg)) begin
+        $display("Running Test with %s operation(s).", op_arg);
+        case (op_arg)
+            "ADD": opc = ADD;
+            "SUB": opc = SUB;
+            "DIV": opc = DIV;
+            "MUL": opc = MUL;
+            default: opc = opc.first();
+        endcase
+    end else opc = ADD;
+end
 
 initial begin
     op1 = new();
@@ -71,211 +219,35 @@ initial begin
     op2_case = op2_case.first;
     sign_tc = '0;
 
-///////////////////////
-// FULL ADD/SUB TEST //
-///////////////////////
+// Divider Signals Initialization
+    clk = 1'b0;
+    repeat (1) @(negedge clk) rstn = 1'b0;            // assert reset
+    repeat (1) @(negedge clk) rstn = 1'b1;
+    rm = 2'b0;
+    ena = 1'b1;
+    fdiv = 1'b1;
 
-// FIXME: Need to implement multiply and divide in this later.
-// and need to change opcode handling.
-
-// Corner Case Testing
-// do begin
-//     do begin
-//         for (sign_tc = 0; sign_tc < 8; sign_tc++) begin
-//             for (int i = 0; i < NTESTS; i++) begin
-//                 generateCornerCase(op1, op2, exp, op1_case, op2_case, sign_tc[1], sign_tc[0], opcode_t'(sign_tc[2]));
-//                 opcode = sign_tc[2];
-//                 sign1 = op1.getSign();
-//                 sign2 = op2.getSign();
-//                 exp1 = op1.getExponent();
-//                 exp2 = op2.getExponent();
-//                 sig1 = op1.getSignificand();
-//                 sig2 = op2.getSignificand();
-
-//                 #150;
-//                 out.setBits(fp_out);
-//                 checkResults(op1, op2, out, exp, opcode_t'(sign_tc[2]), err);
-//                 if (err) begin 
-//                     err_count += 1;
-//                     $display("EXP: %0b", exp.bitsToString);
-//                     $display("OUT: %0b", out.bitsToString);
-//                 end
-//                 checkErrorCode(exp, err_o);
-//                 test_count += 1;
-//                 // singleTestCase(op1, op2, exp, out, opcode, sign1, sign2, exp1, exp2, sig1, sig2, fp_out, err_o, op1_case, op2_case, sign_tc[2], sign_tc[1], sign_tc[0]);
-//             end
-//         end
-//         op2_case = op2_case.next;
-//     end while(op2_case != op2_case.first);
-//     op1_case = op1_case.next;
-// end while(op1_case != op1_case.first);
-
-
-for (int i = 0; i < (NTESTS * 512); i++) begin
-    opc = opcode_t'($urandom_range(1,0));
-    generateRandCase(op1, op2, exp, opc);
-    opcode = bit'(opc);
-    sign1 = op1.getSign();
-    sign2 = op2.getSign();
-    exp1 = op1.getExponent();
-    exp2 = op2.getExponent();
-    sig1 = op1.getSignificand();
-    sig2 = op2.getSignificand();
-
-    #150;
-    out.setBits(fp_out);
-    checkResults(op1, op2, out, exp, opc, err);
-    if (err) begin 
-        err_count += 1;
-        $display("EXP: %0b", exp.bitsToString);
-        $display("OUT: %0b", out.bitsToString);
-    end
-    checkErrorCode(exp, err_o);
-    test_count += 1;
+///////////////////
+// GENERIC TESTS //
+///////////////////
+if (op_arg == "ALL") begin
+    do begin 
+        `ifdef CORNERCASES
+            testCornerCases();
+        `endif
+        opc = opc.next;
+    end while(opc != opc.first);
+end else begin
 end
+opc = opcode_t'(OP);
 
+`ifdef CORNERCASES
+    testCornerCases();
+`endif
 
-//////////////////////////////////
-// DIV Test -- Now Generic Test //
-//////////////////////////////////
-
-    // clk = 1'b0;
-    // // assert reset
-    // rstn = 1'b0;
-    // #10 rstn = 1'b1;
-    // ena = 1'b0;
-    // fdiv = 1'b0;
-    // rm = 2'b0;
-    // ena = 1'b1;
-    // fdiv = 1'b1;
-    // sign_tc = 0;
-    // opc = ADD;
-
-    // do begin
-    //     do begin
-    //         for (sign_tc = 0; sign_tc < 4; sign_tc++) begin
-    //             for (int i = 0; i < NTESTS; i++) begin
-    //                 generateCornerCase(op1, op2, exp, op1_case, op2_case, sign_tc[1], sign_tc[0], opc);
-                    
-    //                 // Add/Sub Inputs
-    //                 opcode = bit'(opc);
-    //                 sign1 = op1.getSign();
-    //                 sign2 = op2.getSign();
-    //                 exp1 = op1.getExponent();
-    //                 exp2 = op2.getExponent();
-    //                 sig1 = op1.getSignificand();
-    //                 sig2 = op2.getSignificand();
-
-    //                 // Mult/Div Specific Input Handling
-    //                 if (opc == DIV) begin
-    //                     wait(!busy);
-    //                     a = {sign1, exp1, sig1};    // Div Input
-    //                     b = {sign2, exp2, sig2};    // Div Input
-    //                 end else if (opc == MUL) begin
-    //                     in_A = {sign1, exp1, sig1};                   // Mult Input
-    //                     in_B = {sign2, exp2, sig2};                   // Mult Input
-    //                     repeat (1) @(negedge clk);
-    //                     strb_A = 1'b1;
-    //                     strb_B = 1'b1;
-    //                     repeat (1) @(negedge clk);
-    //                     strb_A = 1'b0;
-    //                     strb_B = 1'b0;
-    //                 end
-
-
-    //                 // Op Code Specific Waits and Outputs
-    //                 case (opc)
-    //                     ADD: begin 
-    //                         #150;
-    //                         out.setBits(fp_out);
-    //                     end
-    //                     SUB: begin
-    //                         #50;
-    //                         out.setBits(fp_out);
-    //                     end
-    //                     MUL: begin 
-    //                         out.setBits(output_prod);
-    //                     end
-    //                     DIV: begin
-    //                         #300;
-    //                         wait(!busy);
-    //                         out.setBits(s);
-    //                     end
-    //                 endcase
-    //                 checkResults(op1, op2, out, exp, opc, err);
-    //                 if (err) begin 
-    //                     err_count += 1;
-    //                     $display("EXP: %0b", exp.bitsToString);
-    //                     $display("OUT: %0b", out.bitsToString);
-    //                 end
-    //                 checkErrorCode(exp, err_o);
-    //                 test_count += 1;
-    //             end
-    //         end
-    //         op2_case = op2_case.next;
-    //     end while(op2_case != op2_case.first);
-    //     op1_case = op1_case.next;
-    // end while(op1_case != op1_case.first);
-
-
-//////////////
-// MUL Test //
-//////////////
-
-    // clk = 1'b0;
-    // // assert reset
-    // reset_n = 1'b0;
-    // #10 reset_n = 1'b1;
-    // opc = MUL;
-
-
-    // do begin
-    //     do begin
-    //         for (sign_tc = 0; sign_tc < 4; sign_tc++) begin
-    //             for (int i = 0; i < NTESTS; i++) begin
-    //                 $display("Generating Test Case");
-    //                 generateCornerCase(op1, op2, exp, op1_case, op2_case, sign_tc[1], sign_tc[0], opc);
-    //                 sign1 = op1.getSign();
-    //                 sign2 = op2.getSign();
-    //                 exp1 = op1.getExponent();
-    //                 exp2 = op2.getExponent();
-    //                 sig1 = op1.getSignificand();
-    //                 sig2 = op2.getSignificand();
-    //                 in_A = {sign1, exp1, sig1};
-    //                 in_B = {sign2, exp2, sig2};
-    //                 #1;
-    //                 $display("Setting Strobe Signals");
-    //                 strb_A = 1'b1;
-    //                 strb_B = 1'b1;
-    //                 repeat (1) @(negedge clk);
-    //                 strb_A = 1'b0;
-    //                 strb_B = 1'b0;
-
-    //                 #300;
-    //                 $display("Waiting for output strobe");
-    //                 wait(output_prod_stb);
-    //                 $display("Finished waiting, setting out");
-    //                 out.setBits(output_prod);
-    //                 out_prod_ack = 1'b1;
-    //                 repeat (1) @(negedge clk);
-    //                 out_prod_ack = 1'b0;
-    //                 $display("Checking Results");
-    //                 checkResults(op1, op2, out, exp, opc, err);
-    //                 if (err) begin 
-    //                     err_count += 1;
-    //                     $display("EXP: %0b", exp.bitsToString);
-    //                     $display("OUT: %0b", out.bitsToString);
-    //                 end
-    //                 $display("Checking Error Code");
-    //                 checkErrorCode(exp, err_o);
-    //                 test_count += 1;
-    //             end
-    //         end
-    //         op2_case = op2_case.next;
-    //     end while(op2_case != op2_case.first);
-    //     op1_case = op1_case.next;
-    // end while(op1_case != op1_case.first);
-
+`ifdef RANDCASES
+    testRandCases();
+`endif
 
     $finish();
 end
